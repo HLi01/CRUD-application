5144d6d489c9446247a32e2b479b3185bc1fac23
HLi01 <hermanl@stud.uni-obuda.hu>
2021-11-15 15:59:45 +0100

    Created, and wrote methods of  DriverController

---
+++ J2RXEK_HFT_2021221.Endpoint/Controllers/DriverController.cs
@@ -0,0 +1,58 @@
+﻿using J2RXEK_HFT_2021221.Logic;
+using J2RXEK_HFT_2021221.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace J2RXEK_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DriverController : ControllerBase
+    {
+        IDriverLogic dl;
+        public DriverController(IDriverLogic dl)
+        {
+            this.dl = dl;
+        }
+
+        // GET: /driver
+        [HttpGet]
+        public IEnumerable<Driver> Get()
+        {
+            return dl.ReadAll();
+        }
+
+        // GET /driver/
+        [HttpGet("{id}")]
+        public Driver Get(int id)
+        {
+            return dl.Read(id);
+        }
+
+        // POST /driver
+        [HttpPost]
+        public void Post([FromBody] Driver value)
+        {
+            dl.Create(value);
+        }
+
+        // PUT /driver
+        [HttpPut("{id}")]
+        public void Put([FromBody] Driver value)
+        {
+            dl.Update(value);
+        }
+
+        // DELETE /driver/
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            dl.Delete(id);
+        }
+    }
+}
